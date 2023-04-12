8bda9d9ec03f60738254d8bee32f5c5a56bfbfdf
bryqueaz <bryanquesa@gmail.com>
2023-04-12 13:51:56 -0600

    Create proyecto_database_structure.sql

---
+++ mysqldump/proyecto_database_structure.sql
@@ -0,0 +1,220 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: proyecto
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `queue`
+--
+
+DROP TABLE IF EXISTS `queue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `queue` (
+  `name` varchar(128) NOT NULL,
+  `musiconhold` varchar(128) DEFAULT NULL,
+  `announce` varchar(128) DEFAULT NULL,
+  `context` varchar(128) DEFAULT NULL,
+  `timeout` int DEFAULT NULL,
+  `monitor_join` tinyint(1) DEFAULT NULL,
+  `monitor_format` varchar(128) DEFAULT NULL,
+  `queue_youarenext` varchar(128) DEFAULT NULL,
+  `queue_thereare` varchar(128) DEFAULT NULL,
+  `queue_callswaiting` varchar(128) DEFAULT NULL,
+  `queue_holdtime` varchar(128) DEFAULT NULL,
+  `queue_minutes` varchar(128) DEFAULT NULL,
+  `queue_seconds` varchar(128) DEFAULT NULL,
+  `queue_lessthan` varchar(128) DEFAULT NULL,
+  `queue_thankyou` varchar(128) DEFAULT NULL,
+  `queue_reporthold` varchar(128) DEFAULT NULL,
+  `announce_frequency` int DEFAULT NULL,
+  `announce_round_seconds` int DEFAULT NULL,
+  `announce_holdtime` varchar(128) DEFAULT NULL,
+  `retry` int DEFAULT NULL,
+  `wrapuptime` int DEFAULT NULL,
+  `maxlen` int DEFAULT NULL,
+  `servicelevel` int DEFAULT NULL,
+  `strategy` varchar(128) DEFAULT NULL,
+  `joinempty` varchar(128) DEFAULT NULL,
+  `leavewhenempty` varchar(128) DEFAULT NULL,
+  `eventmemberstatus` tinyint(1) DEFAULT NULL,
+  `eventwhencalled` tinyint(1) DEFAULT NULL,
+  `reportholdtime` tinyint(1) DEFAULT NULL,
+  `memberdelay` int DEFAULT NULL,
+  `weight` int DEFAULT NULL,
+  `timeoutrestart` tinyint(1) DEFAULT NULL,
+  `ringinuse` tinyint(1) DEFAULT NULL,
+  `setinterfacevar` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `queue_log`
+--
+
+DROP TABLE IF EXISTS `queue_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `queue_log` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `time` varchar(26) NOT NULL DEFAULT '',
+  `callid` varchar(40) NOT NULL DEFAULT '',
+  `queuename` varchar(20) NOT NULL DEFAULT '',
+  `agent` varchar(20) NOT NULL DEFAULT '',
+  `event` varchar(20) NOT NULL DEFAULT '',
+  `data` varchar(100) NOT NULL DEFAULT '',
+  `data1` varchar(40) NOT NULL DEFAULT '',
+  `data2` varchar(40) NOT NULL DEFAULT '',
+  `data3` varchar(40) NOT NULL DEFAULT '',
+  `data4` varchar(40) NOT NULL DEFAULT '',
+  `data5` varchar(40) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `queue` (`queuename`),
+  KEY `event` (`event`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `queue_member`
+--
+
+DROP TABLE IF EXISTS `queue_member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `queue_member` (
+  `membername` varchar(40) DEFAULT NULL,
+  `queue_name` varchar(128) DEFAULT NULL,
+  `interface` varchar(128) DEFAULT NULL,
+  `penalty` int DEFAULT NULL,
+  `paused` int DEFAULT NULL,
+  UNIQUE KEY `queue_interface` (`queue_name`,`interface`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sip`
+--
+
+DROP TABLE IF EXISTS `sip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sip` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `ipaddr` varchar(15) DEFAULT NULL,
+  `port` int DEFAULT NULL,
+  `regseconds` int DEFAULT NULL,
+  `defaultuser` varchar(10) DEFAULT NULL,
+  `fullcontact` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `regserver` varchar(20) DEFAULT NULL,
+  `useragent` varchar(20) DEFAULT NULL,
+  `lastms` int DEFAULT NULL,
+  `host` varchar(40) DEFAULT NULL,
+  `type` enum('friend','user','peer') DEFAULT NULL,
+  `context` varchar(40) DEFAULT NULL,
+  `permit` varchar(40) DEFAULT NULL,
+  `deny` varchar(40) DEFAULT NULL,
+  `secret` varchar(40) DEFAULT NULL,
+  `md5secret` varchar(40) DEFAULT NULL,
+  `remotesecret` varchar(40) DEFAULT NULL,
+  `transport` enum('udp','tcp','udp,tcp','tcp,udp') DEFAULT NULL,
+  `dtmfmode` enum('rfc2833','info','shortinfo','inband','auto') DEFAULT NULL,
+  `directmedia` enum('yes','no','nonat','update') DEFAULT NULL,
+  `nat` enum('yes','no','never','route','force_rport,comedia') DEFAULT NULL,
+  `callgroup` varchar(40) DEFAULT NULL,
+  `pickupgroup` varchar(40) DEFAULT NULL,
+  `language` varchar(40) DEFAULT NULL,
+  `allow` varchar(40) DEFAULT NULL,
+  `disallow` varchar(40) DEFAULT NULL,
+  `insecure` varchar(40) DEFAULT NULL,
+  `trustrpid` enum('yes','no') DEFAULT NULL,
+  `progressinband` enum('yes','no','never') DEFAULT NULL,
+  `promiscredir` enum('yes','no') DEFAULT NULL,
+  `useclientcode` enum('yes','no') DEFAULT NULL,
+  `accountcode` varchar(40) DEFAULT NULL,
+  `setvar` varchar(40) DEFAULT NULL,
+  `callerid` varchar(40) DEFAULT NULL,
+  `amaflags` varchar(40) DEFAULT NULL,
+  `callcounter` enum('yes','no') DEFAULT NULL,
+  `busylevel` int DEFAULT NULL,
+  `allowoverlap` enum('yes','no') DEFAULT NULL,
+  `allowsubscribe` enum('yes','no') DEFAULT NULL,
+  `videosupport` enum('yes','no') DEFAULT NULL,
+  `maxcallbitrate` int DEFAULT NULL,
+  `rfc2833compensate` enum('yes','no') DEFAULT NULL,
+  `mailbox` varchar(40) DEFAULT NULL,
+  `session-timers` enum('accept','refuse','originate') DEFAULT NULL,
+  `session-expires` int DEFAULT NULL,
+  `session-minse` int DEFAULT NULL,
+  `session-refresher` enum('uac','uas') DEFAULT NULL,
+  `t38pt_usertpsource` varchar(40) DEFAULT NULL,
+  `regexten` varchar(40) DEFAULT NULL,
+  `fromdomain` varchar(40) DEFAULT NULL,
+  `fromuser` varchar(40) DEFAULT NULL,
+  `qualify` varchar(40) DEFAULT NULL,
+  `defaultip` varchar(40) DEFAULT NULL,
+  `rtptimeout` int DEFAULT NULL,
+  `rtpholdtimeout` int DEFAULT NULL,
+  `sendrpid` enum('yes','no') DEFAULT NULL,
+  `outboundproxy` varchar(40) DEFAULT NULL,
+  `callbackextension` varchar(40) DEFAULT NULL,
+  `registertrying` enum('yes','no') DEFAULT NULL,
+  `timert1` int DEFAULT NULL,
+  `timerb` int DEFAULT NULL,
+  `qualifyfreq` int DEFAULT NULL,
+  `constantssrc` enum('yes','no') DEFAULT NULL,
+  `contactpermit` varchar(40) DEFAULT NULL,
+  `contactdeny` varchar(40) DEFAULT NULL,
+  `usereqphone` enum('yes','no') DEFAULT NULL,
+  `textsupport` enum('yes','no') DEFAULT NULL,
+  `faxdetect` enum('yes','no') DEFAULT NULL,
+  `buggymwi` enum('yes','no') DEFAULT NULL,
+  `auth` varchar(40) DEFAULT NULL,
+  `fullname` varchar(40) DEFAULT NULL,
+  `trunkname` varchar(40) DEFAULT NULL,
+  `cid_number` varchar(40) DEFAULT NULL,
+  `callingpres` enum('allowed_not_screened','allowed_passed_screen','allowed_failed_screen','allowed','prohib_not_screened','prohib_passed_screen','prohib_failed_screen','prohib') DEFAULT NULL,
+  `mohinterpret` varchar(40) DEFAULT NULL,
+  `mohsuggest` varchar(40) DEFAULT NULL,
+  `parkinglot` varchar(40) DEFAULT NULL,
+  `hasvoicemail` enum('yes','no') DEFAULT NULL,
+  `subscribemwi` enum('yes','no') DEFAULT NULL,
+  `vmexten` varchar(40) DEFAULT NULL,
+  `autoframing` enum('yes','no') DEFAULT NULL,
+  `rtpkeepalive` int DEFAULT NULL,
+  `call-limit` int DEFAULT NULL,
+  `g726nonstandard` enum('yes','no') DEFAULT NULL,
+  `ignoresdpversion` enum('yes','no') DEFAULT NULL,
+  `allowtransfer` enum('yes','no') DEFAULT NULL,
+  `dynamic` enum('yes','no') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `ipaddr` (`ipaddr`,`port`),
+  KEY `host` (`host`,`port`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-12 19:50:46
